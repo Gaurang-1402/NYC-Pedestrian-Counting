027a37a1d9193251ef24c698c88787cb74039692
Gaurang ESP Ubuntu <gr2159@nyu.edu>
2023-07-12 15:20:02 -0400

    make things network (TTN) lorawan communication work, add hello world example

--- esp32/NYC_pedestrian_counter/main/main.cpp
+++ esp32/NYC_pedestrian_counter/main/main.cpp
@@ -1,121 +1,121 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_event.h"
+#include "driver/gpio.h"
+#include "nvs_flash.h"
+#include <string.h>
+#include "TheThingsNetwork.h"
 
+#include "config.h"
+
+/* Camera and YOLO setup */
 
 #include "app_lcd.h"
 #include "app_camera.h"
 #include "algo_yolo.hpp"
 
-#include <stdio.h>
-#include <inttypes.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "lora.h"
 
-extern "C" {
+/* LoRa setup */
 
-	static QueueHandle_t xQueueAIFrame = NULL;
-	static QueueHandle_t xQueueLCDFrame = NULL;
+// NOTE:
+// The LoRaWAN frequency and the radio chip must be configured by running 'idf.py menuconfig'.
+// Go to Components / The Things Network, select the appropriate values and save.
 
-	#if CONFIG_SENDER
-	void task_tx(void *pvParameters)
-	{
-		ESP_LOGI(pcTaskGetName(NULL), "Start");
-		uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
-		while(1) {
-			TickType_t nowTick = xTaskGetTickCount();
-			int send_len = sprintf((char *)buf,"Hello World!! %"PRIu32, nowTick);
-			lora_send_packet(buf, send_len);
-			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...", send_len);
-			vTaskDelay(pdMS_TO_TICKS(5000));
-		} // end while
-	}
-	#endif // CONFIG_SENDER
+// Copy the below hex strings from the TTN console (Applications > Your application > End devices
+// > Your device > Activation information)
 
-	#if CONFIG_RECEIVER
-	void task_rx(void *pvParameters)
-	{
-		ESP_LOGI(pcTaskGetName(NULL), "Start");
-		uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
-		while(1) {
-			lora_receive(); // put into receive mode
-			if (lora_received()) {
-				int receive_len = lora_receive_packet(buf, sizeof(buf));
-				ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", receive_len, receive_len, buf);
-			} // end if
-			vTaskDelay(1); // Avoid WatchDog alerts
-		} // end while
-	}
-	#endif // CONFIG_RECEIVER
+// AppEUI (sometimes called JoinEUI)
+const char *appEui = APPEUI;
+// DevEUI
+const char *devEui = DEVEUI;
+// AppKey
+const char *appKey = APPKEY;
 
-	void app_main()
-	{
-		if (lora_init() == 0) {
-			ESP_LOGE(pcTaskGetName(NULL), "Does not recognize the module");
-			while(1) {
-				vTaskDelay(1);
-			}
 
-		}
+static TheThingsNetwork ttn;
 
-		xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
-		xQueueLCDFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+const unsigned TX_INTERVAL = 30;
+static uint8_t msgData[] = "Hello, world";
 
-		register_camera(PIXFORMAT_RGB565, FRAMESIZE_240X240, 2, xQueueAIFrame);
-		register_algo_yolo(xQueueAIFrame, NULL, NULL, xQueueLCDFrame, false);
-		register_lcd(xQueueLCDFrame, NULL, true);
 
-	#if CONFIG_169MHZ
-		ESP_LOGI(pcTaskGetName(NULL), "Frequency is 169MHz");
-		lora_set_frequency(169e6); // 169MHz
-	#elif CONFIG_433MHZ
-		ESP_LOGI(pcTaskGetName(NULL), "Frequency is 433MHz");
-		lora_set_frequency(433e6); // 433MHz
-	#elif CONFIG_470MHZ
-		ESP_LOGI(pcTaskGetName(NULL), "Frequency is 470MHz");
-		lora_set_frequency(470e6); // 470MHz
-	#elif CONFIG_866MHZ
-		ESP_LOGI(pcTaskGetName(NULL), "Frequency is 866MHz");
-		lora_set_frequency(866e6); // 866MHz
-	#elif CONFIG_915MHZ
-		ESP_LOGI(pcTaskGetName(NULL), "Frequency is 915MHz");
-		lora_set_frequency(915e6); // 915MHz
-	#elif CONFIG_OTHER
-		ESP_LOGI(pcTaskGetName(NULL), "Frequency is %dMHz", CONFIG_OTHER_FREQUENCY);
-		long frequency = CONFIG_OTHER_FREQUENCY * 1000000;
-		lora_set_frequency(frequency);
-	#endif
+void sendMessages(void* pvParameter)
+{
+    while (1) {
+        printf("Sending message...\n");
+        TTNResponseCode res = ttn.transmitMessage(msgData, sizeof(msgData) - 1);
+        printf(res == kTTNSuccessfulTransmission ? "Message sent.\n" : "Transmission failed.\n");
 
-		lora_enable_crc();
+        vTaskDelay(TX_INTERVAL * pdMS_TO_TICKS(1000));
+    }
+}
 
-		int cr = 1;
-		int bw = 7;
-		int sf = 7;
-	#if CONFIF_ADVANCED
-		cr = CONFIG_CODING_RATE
-		bw = CONFIG_BANDWIDTH;
-		sf = CONFIG_SF_RATE;
-	#endif
+void messageReceived(const uint8_t* message, size_t length, ttn_port_t port)
+{
+    printf("Message of %d bytes received on port %d:", length, port);
+    for (int i = 0; i < length; i++)
+        printf(" %02x", message[i]);
+    printf("\n");
+}
 
-		lora_set_coding_rate(cr);
-		//lora_set_coding_rate(CONFIG_CODING_RATE);
-		//cr = lora_get_coding_rate();
-		ESP_LOGI(pcTaskGetName(NULL), "coding_rate=%d", cr);
+extern "C"
 
-		lora_set_bandwidth(bw);
-		//lora_set_bandwidth(CONFIG_BANDWIDTH);
-		//int bw = lora_get_bandwidth();
-		ESP_LOGI(pcTaskGetName(NULL), "bandwidth=%d", bw);
+{ 
 
-		lora_set_spreading_factor(sf);
-		//lora_set_spreading_factor(CONFIG_SF_RATE);
-		//int sf = lora_get_spreading_factor();
-		ESP_LOGI(pcTaskGetName(NULL), "spreading_factor=%d", sf);
+static QueueHandle_t xQueueAIFrame = NULL;
+static QueueHandle_t xQueueLCDFrame = NULL;
 
-	#if CONFIG_SENDER
-		xTaskCreate(&task_tx, "task_tx", 1024*3, NULL, 5, NULL);
-	#endif
-	#if CONFIG_RECEIVER
-		xTaskCreate(&task_rx, "task_rx", 1024*3, NULL, 5, NULL);
-	#endif
-	}
-} // extern "C"
+
+    
+    void app_main(void) {
+    esp_err_t err;
+    // Initialize the GPIO ISR handler service
+    err = gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
+    ESP_ERROR_CHECK(err);
+    
+    // Initialize the NVS (non-volatile storage) for saving and restoring the keys
+    err = nvs_flash_init();
+    ESP_ERROR_CHECK(err);
+
+    // Initialize SPI bus
+    spi_bus_config_t spi_bus_config;
+    memset(&spi_bus_config, 0, sizeof(spi_bus_config));
+    spi_bus_config.miso_io_num = TTN_PIN_SPI_MISO;
+    spi_bus_config.mosi_io_num = TTN_PIN_SPI_MOSI;
+    spi_bus_config.sclk_io_num = TTN_PIN_SPI_SCLK;
+    err = spi_bus_initialize(TTN_SPI_HOST, &spi_bus_config, TTN_SPI_DMA_CHAN);
+    ESP_ERROR_CHECK(err);
+
+    // Configure the SX127x pins
+    ttn.configurePins(TTN_SPI_HOST, TTN_PIN_NSS, TTN_PIN_RXTX, TTN_PIN_RST, TTN_PIN_DIO0, TTN_PIN_DIO1);
+
+    // The below line can be commented after the first run as the data is saved in NVS
+    ttn.provision(devEui, appEui, appKey);
+
+    // Register callback for received messages
+    ttn.onMessage(messageReceived);
+
+//    ttn.setAdrEnabled(false);
+//    ttn.setDataRate(kTTNDataRate_US915_SF7);
+//    ttn.setMaxTxPower(14);
+
+    printf("Joining...\n");
+    if (ttn.join())
+    {
+        printf("Joined.\n");
+        xTaskCreate(sendMessages, "send_messages", 1024 * 4, (void* )0, 3, nullptr);
+
+        /* Camera and YOLO */
+        xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+        xQueueLCDFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+
+        register_camera(PIXFORMAT_RGB565, FRAMESIZE_240X240, 2, xQueueAIFrame);
+        register_algo_yolo(xQueueAIFrame, NULL, NULL, xQueueLCDFrame, false);
+        register_lcd(xQueueLCDFrame, NULL, true);
+
+    }
+    else
+    {
+        printf("Join failed. Goodbye\n");
+    }
+}
+
+}
